2db2df478d49c18da4686a8274f45796b5604472
ymekuria <ymekuria@gmail.com>
2015-12-04 15:21:29 -0800

    'Added a gameboard and an enemy'

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,14 @@
 // start slingin' some d3 here.
+var width = 800;
+var height = 500;
+
+var board = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .classed('board', true);
+
+var player = board.append('circle')
+  .attr('cx', width/2)
+  .attr('cy', height/2)
+  .attr('r', 50)
+  .attr('fill', 'blue')

792626ced193416d55df22ac315969e12a0503fe
ymekuria <ymekuria@gmail.com>
2015-12-04 19:37:00 -0800

    Implemented Player dragging

--- client/watchout.js
+++ client/watchout.js
@@ -1,18 +1,25 @@
 // start slingin' some d3 here.
 var width = 800;
 var height = 500;
-var enemies = 50;
+var enemies = 5;
 var board = d3.select('body').append('svg')
   .attr('width', width)
   .attr('height', height)
   .classed('board', true);
 
+var drag = d3.behavior.drag()  
+ .on('drag', function() { player.attr('cx', d3.event.x)
+                                .attr('cy', d3.event.y); })
+ 
 var player = board.append('circle')
   .attr('cx', width/2)
   .attr('cy', height/2)
   .attr('r', 25)
   .attr('fill', 'blue')
+  .call(drag)
   .classed('player', true); 
+
+
   
 var makeEnemyArray = function(){
   var array = []
@@ -35,9 +42,8 @@ var makeEnemyArray = function(){
 var enemyMove = function(array){
   var selection = d3.select('svg').selectAll('.enemy')
     .data(array, function(d) {return d.id})
-    
-    
-  selection.transition().duration(1500)
+      
+  selection.transition().duration(1000)
     .attr('cx', function(d){return d.cx})
     .attr('cy', function(d){return d.cy});   
 
@@ -52,7 +58,7 @@ var enemyMove = function(array){
 
 }
 enemyMove(makeEnemyArray());
-setInterval(function(){enemyMove(makeEnemyArray())}, 2000);
+setInterval(function(){enemyMove(makeEnemyArray())}, 1000);
 
 
 

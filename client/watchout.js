2ab7b0c22120e998274ab5744dfd497ecb88667d
ymekuria <ymekuria@gmail.com>
2015-12-04 17:36:13 -0800

    'Refactored enemy move function'

--- client/watchout.js
+++ client/watchout.js
@@ -1,41 +1,53 @@
 // start slingin' some d3 here.
 var width = 800;
 var height = 500;
-var enemies = 10
+var enemies = 5
 var board = d3.select('body').append('svg')
   .attr('width', width)
   .attr('height', height)
   .classed('board', true);
 
-// var player = board.append('circle')
-//   .attr('cx', width/2)
-//   .attr('cy', height/2)
-//   .attr('r', 25)
-//   .attr('fill', 'blue')
-//   .classed('player', true); 
+var player = board.append('circle')
+  .attr('cx', width/2)
+  .attr('cy', height/2)
+  .attr('r', 25)
+  .attr('fill', 'blue')
+  .classed('player', true); 
   
-var enemyArr = []
-for(var i = 0; i < 10; i++){
-  // var cx = Math.random() * width;
-  // var cy = Math.random() * height;
-  enemyArr.push(i);
+var makeEnemyArray = function(){
+  var array = []
+  var makeEnemy = function(i, cx, cy){
+    var enemyObj = {}
+    enemyObj.id = i;
+    enemyObj.cx = cx;
+    enemyObj.cy = cy;
+    return enemyObj;
+  }
+
+  for(var i = 0; i < enemies; i++){
+    var cx = Math.random() * width;
+    var cy = Math.random() * height;
+    array[i] = makeEnemy(i, cx, cy);
+  }
+  return array
 }
 
 var enemyMove = function(array){
-  var selection = d3.select('svg').selectAll('circle')
-    .data(array, function(d) {return d})
-    .attr('cx', function(){return Math.random() * width})
-    .attr('cy', function(){return Math.random() * height})
-
+  var selection = d3.select('svg').selectAll('.enemy')
+    
+    .data(array, function(d) {return d.id}).append('circle')
+    .attr('cx', function(d){return d.cx})
+    .attr('cy', function(d){return d.cy})
+    
   selection.enter().append('circle')
-    .attr('cx', function(){return Math.random() * width})
-    .attr('cy', function(){return Math.random() * height})
+    .attr('cx', function(d){return d.cx})
+    .attr('cy', function(d){return d.cy})
     .attr('r', 10)
     .attr('fill', 'red')
-    .classed('enemy', true);
+    .classed('enemy', true)
 }
 
-enemyMove(enemyArr);
+enemyMove(makeEnemyArray());
 
 
 
